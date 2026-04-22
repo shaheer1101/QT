3592e0710b6e81edc5497bf6d9f6c13efae43ab9
shaheer1101 <ms9612684@gmail.com>
2026-04-22 23:29:40 +0500

    c folder

--- src/components/screens/OutroScreen.jsx
+++ src/components/screens/OutroScreen.jsx
@@ -5,7 +5,7 @@ export default function OutroScreen() {
       className="flex flex-col items-center justify-center p-6 text-center"
     >
       <div className="mb-4">
-        <img src="/gifs/happy.gif" alt="happy gif" className="w-40" />
+        <img src="/Gifs_Final/happy.gif" alt="happy gif" className="w-40" />
       </div>
 
       <h1 className="text-4xl md:text-5xl font-dancing text-rose-500/85 font-semibold mb-3">You're the best</h1>
